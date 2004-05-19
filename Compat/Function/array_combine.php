9243172f8ed6750cd0cb3e22cddbe80269ed096a
Aidan Lister <aidan@php.net>
2004-05-19 12:06:20 +0000

    Added license block and revision info
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@158985 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/array_combine.php
+++ Compat/Function/array_combine.php
@@ -1,4 +1,24 @@
 <?php
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3 of the PHP license,         |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://au.php.net/license/3_0.txt.                                   |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Aidan Lister <aidan@php.net>                                 |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+
+
 /**
  * Replace array_combine()
  * PHP 5
