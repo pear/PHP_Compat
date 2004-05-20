d966ea7146e50714521100e696c210e5f6d3285d
Aidan Lister <aidan@php.net>
2004-05-20 13:36:57 +0000

    Initial Commit
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@159131 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Compat/Function/str_split.php
@@ -0,0 +1,42 @@
+<?php
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3 of the PHP license,         |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://au.php.net/license/3_0.txt.                                   |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Aidan Lister <aidan@php.net>                                 |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+
+
+/**
+ * Replace str_split()
+ *
+ * PHP 5
+ *
+ * http://php.net/function.str_split
+ *
+ * @author        Paul Gregg <phpcode@pgregg.com>
+ * @author        Aidan Lister <aidan@php.net>
+ * @version       1.0
+ */
+if (!function_exists('str_split'))
+{
+    function str_split ($string, $chunksize = 1)
+    {
+        preg_match_all('/(' . str_repeat('.', $chunksize) . ')/Uims', $string, $matches);
+        return $matches[1];
+    }
+}
+
+?>
