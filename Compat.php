4ddac22276f5f81ad99d22899035f6c3b604299e
Aidan Lister <aidan@php.net>
2004-05-28 01:18:48 +0000

    Fixed/Changed the docblock headers and license info
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@159834 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat.php
+++ Compat.php
@@ -1,18 +1,19 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
 // +----------------------------------------------------------------------+
 // | PHP Version 4                                                        |
 // +----------------------------------------------------------------------+
 // | Copyright (c) 1997-2004 The PHP Group                                |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 3 of the PHP license,         |
+// | This source file is subject to version 3.0 of the PHP license,       |
 // | that is bundled with this package in the file LICENSE, and is        |
 // | available at through the world-wide-web at                           |
-// | http://au.php.net/license/3_0.txt.                                   |
+// | http://www.php.net/license/3_0.txt.                                  |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Author: Aidan Lister <aidan@php.net>                                 |
+// | Authors: Aidan Lister <aidan@php.net>                                |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -28,6 +29,8 @@
  * Optionally, you may simply include the file.
  *   e.g. require_once 'PHP/Compat/Function/scandir.php';
  *
+ * @category       PHP
+ * @package        PHP_Compat
  * @version        0.1
  * @author         Aidan Lister <aidan@php.net>
  */
--- Compat/Constant/STDERR.php
+++ Compat/Constant/STDERR.php
@@ -1,18 +1,19 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
 // +----------------------------------------------------------------------+
 // | PHP Version 4                                                        |
 // +----------------------------------------------------------------------+
 // | Copyright (c) 1997-2004 The PHP Group                                |
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 3 of the PHP license,         |
+// | This source file is subject to version 3.0 of the PHP license,       |
 // | that is bundled with this package in the file LICENSE, and is        |
 // | available at through the world-wide-web at                           |
-// | http://au.php.net/license/3_0.txt.                                   |
+// | http://www.php.net/license/3_0.txt.                                  |
 // | If you did not receive a copy of the PHP license and are unable to   |
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Author: Aidan Lister <aidan@php.net>                                 |
+// | Authors: Aidan Lister <aidan@php.net>                                |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -22,12 +23,14 @@
 /**
  * Replace constant STDERR
  *
- * PHP 4.3.0
- * 
- * http://php.net/wrappers
+ * Added in PHP 4.3.0
  *
- * @author        Aidan Lister <aidan@php.net>
- * @version       1.0
+ * @category    PHP
+ * @package     PHP_Compat
+ * @link        http://php.net/wrappers
+ * @author      Aidan Lister <aidan@php.net>
+ * @author      Michael Wallner <mike@php.net>
+ * @version     1.0
  */
 if (!defined('STDERR')) {
     define('STDERR', fopen('php://stderr', 'w'));
