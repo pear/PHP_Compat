14ac08146c6ce9ec4c8a9aaaa559df83482a0ab1
Aidan Lister <aidan@php.net>
2004-05-29 03:23:49 +0000

    Updated docblock tags
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@159932 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat.php
+++ Compat.php
@@ -24,15 +24,14 @@
  * Provides missing functionality in the form of constants and functions
  *   for older versions of PHP
  *
- * The methods in this class should be called statically
- *
  * Optionally, you may simply include the file.
  *   e.g. require_once 'PHP/Compat/Function/scandir.php';
  *
- * @category       PHP
- * @package        PHP_Compat
- * @version        0.1
- * @author         Aidan Lister <aidan@php.net>
+ * @category    PHP
+ * @package     PHP_Compat
+ * @version     0.1
+ * @author      Aidan Lister <aidan@php.net>
+ * @static
  */
 class PHP_Compat
 {
@@ -40,8 +39,8 @@ class PHP_Compat
     /**
      * Load a function, or array of functions
      *
-     * @param string|array $function The function or functions to load.
-     * @return void
+     * @param   string|array    $function The function or functions to load.
+     * @return  bool            true if loaded, false if not
      */
     function loadFunction ($function)
     {
@@ -68,8 +67,8 @@ class PHP_Compat
     /**
      * Load a constant, or array of constants
      *
-     * @param string|array $constant The constant or constants to load.
-     * @return void
+     * @param   string|array    $constant The constant or constants to load.
+     * @return  bool            true if loaded, false if not
      */
     function loadConstant ($constant)
     {
