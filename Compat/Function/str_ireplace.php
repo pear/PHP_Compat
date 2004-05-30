7d88930c94eb558dd2b3de224ebccf7aedcb8e03
Aidan Lister <aidan@php.net>
2004-05-30 15:50:17 +0000

    Tinkered
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@160076 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/str_ireplace.php
+++ Compat/Function/str_ireplace.php
@@ -1,19 +1,19 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 // +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
+// | PHP Version 4														|
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
+// | Copyright (c) 1997-2004 The PHP Group								|
 // +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/3_0.txt.                                  |
+// | This source file is subject to version 3.0 of the PHP license,	   |
+// | that is bundled with this package in the file LICENSE, and is		|
+// | available at through the world-wide-web at						   |
+// | http://www.php.net/license/3_0.txt.								  |
 // | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
+// | obtain it through the world-wide-web, please send a note to		  |
+// | license@php.net so we can mail you a copy immediately.			   |
 // +----------------------------------------------------------------------+
-// | Authors: Aidan Lister <aidan@php.net>                                |
+// | Authors: Aidan Lister <aidan@php.net>								|
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -23,47 +23,55 @@
 /**
  * Replace str_ireplace()
  *
- * Added in PHP 5
- *
  * @category    PHP
  * @package     PHP_Compat
  * @link        http://php.net/function.str_ireplace
  * @author      Aidan Lister <aidan@php.net>
  * @version     1.0
- * @todo		Test, Fix, etc
+ * @added       PHP5
+ * @requires	PHP3
+ * @todo        Test
+ * @todo        Add php replacement errors
  */
 if (!function_exists('str_ireplace'))
 {
 	function stri_replace($find,$replace,$string)
 	{
-		   if(!is_array($find)) $find = array($find);
-		   if(!is_array($replace))
-		   {
-				   if(!is_array($find)) $replace = array($replace);
-				   else
-				   {
-						   // this will duplicate the string into an array the size of $find
-						   $c = count($find);
-						   $rString = $replace;
-						   unset($replace);
-						   for ($i = 0; $i < $c; $i++)
-						   {
-								   $replace[$i] = $rString;
-						   }
-				   }
-		   }
-		   foreach($find as $fKey => $fItem)
-		   {
-				   $between = explode(strtolower($fItem),strtolower($string));
-				   $pos = 0;
-				   foreach($between as $bKey => $bItem)
-				   {
-						   $between[$bKey] = substr($string,$pos,strlen($bItem));
-						   $pos += strlen($bItem) + strlen($fItem);
-				   }
-				   $string = implode($replace[$fKey],$between);
-		   }
-		   return($string);
+		if (!is_array($find)) {
+			$find = array($find);
+		}
+
+		if (!is_array($replace))
+		{
+			if (!is_array($find)) {
+				$replace = array($replace);
+			}
+			else
+			{
+				$c = count($find);
+				$rString = $replace;
+				unset($replace);
+
+				for ($i = 0; $i < $c; $i++) {
+					$replace[$i] = $rString;
+				}
+			}
+		}
+
+		foreach($find as $fKey => $fItem)
+		{
+			$between = explode(strtolower($fItem), strtolower($string));
+			$pos = 0;
+
+			foreach($between as $bKey => $bItem)
+			{
+				$between[$bKey] = substr($string, $pos, strlen($bItem));
+				$pos += strlen($bItem) + strlen($fItem);
+			}
+			$string = implode($replace[$fKey], $between);
+		}
+
+		return($string);
 	}
 }
 
