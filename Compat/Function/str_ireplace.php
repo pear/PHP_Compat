d894e58e59b3397892025c33fffb4db9710933f4
Aidan Lister <aidan@php.net>
2004-05-30 14:36:49 +0000

    Added rough idea of the function from the manual
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@160061 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/str_ireplace.php
+++ Compat/Function/str_ireplace.php
@@ -30,10 +30,41 @@
  * @link        http://php.net/function.str_ireplace
  * @author      Aidan Lister <aidan@php.net>
  * @version     1.0
+ * @todo		Test, Fix, etc
  */
 if (!function_exists('str_ireplace'))
 {
-	// TODO
+	function stri_replace($find,$replace,$string)
+	{
+		   if(!is_array($find)) $find = array($find);
+		   if(!is_array($replace))
+		   {
+				   if(!is_array($find)) $replace = array($replace);
+				   else
+				   {
+						   // this will duplicate the string into an array the size of $find
+						   $c = count($find);
+						   $rString = $replace;
+						   unset($replace);
+						   for ($i = 0; $i < $c; $i++)
+						   {
+								   $replace[$i] = $rString;
+						   }
+				   }
+		   }
+		   foreach($find as $fKey => $fItem)
+		   {
+				   $between = explode(strtolower($fItem),strtolower($string));
+				   $pos = 0;
+				   foreach($between as $bKey => $bItem)
+				   {
+						   $between[$bKey] = substr($string,$pos,strlen($bItem));
+						   $pos += strlen($bItem) + strlen($fItem);
+				   }
+				   $string = implode($replace[$fKey],$between);
+		   }
+		   return($string);
+	}
 }
 
 ?>
