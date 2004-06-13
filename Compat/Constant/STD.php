1fcf3aef3a27c96f6472453e023ced07a03bd2dd
Aidan Lister <aidan@php.net>
2004-06-13 18:16:16 +0000

    Removed tab
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@161280 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Constant/STD.php
+++ Compat/Constant/STD.php
@@ -31,13 +31,13 @@
  * @since       PHP 4.3.0
  */
 if (!defined('STDIN')) {
-	define('STDIN', fopen('php://stdin', 'r'));
+    define('STDIN', fopen('php://stdin', 'r'));
 }
 if (!defined('STDOUT')) {
-	define('STDOUT', fopen('php://stdout', 'w'));
+    define('STDOUT', fopen('php://stdout', 'w'));
 }
 if (!defined('STDERR')) {
-	define('STDERR', fopen('php://stderr', 'w'));
+    define('STDERR', fopen('php://stderr', 'w'));
 }
 
 ?>
