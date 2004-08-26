3995d87219c6e3e5f681f9804df2805bc9cb812e
Aidan Lister <aidan@php.net>
2004-08-26 22:39:21 +0000

    Added str_shuffle
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@167346 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Compat/Function/str_shuffle.php
@@ -0,0 +1,52 @@
+<?php
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Aidan Lister <aidan@php.net>                                |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+
+/**
+ * Replace str_shuffle()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @link        http://php.net/str_shuffle
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     $Revision$
+ * @since       PHP 4.3.0
+ * @require     PHP 4.0.1 (trigger_error)
+ */
+if (!function_exists('str_shuffle'))
+{
+    function str_shuffle($str)
+    {
+        $array = array();
+        for($i = 0, $ii = strlen($str); $i < $ii; $i++) {
+            $array[] = $str[$i];
+        }
+
+        shuffle($array);
+
+        $newstring = '';
+        for($i = 0, $ii = count($array); $i < $ii; $i++) {
+            $newstring .= $array[$i];
+        }
+
+        return $newstring;
+    }
+}
+
+?>
---
+++ tests/function/str_shuffle.phpt
@@ -0,0 +1,16 @@
+--TEST--
+Function -- str_shuffle
+--SKIPIF--
+<?php if (function_exists('str_shuffle')) { echo 'skip'; } ?>
+--FILE--
+<?php
+require_once 'PHP/Compat.php';
+PHP_Compat::loadFunction('str_shuffle');
+
+$string = str_shuffle('ab');
+if ($string == 'ab' || $string == 'ba') {
+    echo "true";
+}
+?>
+--EXPECT--
+true
