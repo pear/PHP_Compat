7ff03245d9505bf511811b9b19d27334603a8611
Aidan Lister <aidan@php.net>
2004-08-26 01:37:47 +0000

    Working but still needs error checking and testing, Thanks Mike!
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@167279 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/convert_uuencode.php
+++ Compat/Function/convert_uuencode.php
@@ -12,7 +12,8 @@
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Authors: Aidan Lister <aidan@php.net>                                |
+// | Authors: Michael Wallner <mike@php.net>                              |
+// |          Aidan Lister <aidan@php.net>                                |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -24,6 +25,7 @@
  * @category    PHP
  * @package     PHP_Compat
  * @link        http://php.net/convert_uuencode
+ * @author      Michael Wallner <mike@php.net>
  * @author      Aidan Lister <aidan@php.net>
  * @version     $Revision$
  * @since       PHP 5
@@ -31,9 +33,40 @@
  */
 if (!function_exists('convert_uuencode'))
 {
-    function convert_uuencode()
+    function convert_uuencode($string)
     {
+        $u = 0;
+        $encoded = null;
+        
+        while ($c = count($bytes = unpack('c*', substr($string, $u, 45)))) {
+            $u += 45;
+            $encoded .= pack('c', $c + 0x20);
 
+            while ($c % 3) {
+                $bytes[++$c] = 0;
+            }
+
+            foreach (array_chunk($bytes, 3) as $b) {
+                $b0 = ($b[0] & 0xFC) >> 2;
+                $b1 = (($b[0] & 0x03) << 4) + (($b[1] & 0xF0) >> 4);
+                $b2 = (($b[1] & 0x0F) << 2) + (($b[2] & 0xC0) >> 6);
+                $b3 = $b[2] & 0x3F;
+                
+                $b0 = $b0 ? $b0 + 0x20 : 0x60;
+                $b1 = $b1 ? $b1 + 0x20 : 0x60;
+                $b2 = $b2 ? $b2 + 0x20 : 0x60;
+                $b3 = $b3 ? $b3 + 0x20 : 0x60;
+                
+                $encoded .= pack('c*', $b0, $b1, $b2, $b3);
+            }
+
+            $encoded .= "\n";
+        }
+        
+        // Add termination characters
+        $encoded .= "\x60\n";
+
+        return $encoded;
     }
 }
 
