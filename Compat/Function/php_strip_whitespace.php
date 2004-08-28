97fbb1b2e00542476b58fec6e91205204199f043
Aidan Lister <aidan@php.net>
2004-08-28 09:28:17 +0000

    oops
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@167459 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Compat/Function/php_strip_whitespace.php
@@ -0,0 +1,55 @@
+<?php
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Aidan Lister <aidan@php.net>                                |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+
+/**
+ * Replace php_strip_whitespace()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @link        http://php.net/php_strip_whitespace
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     $Revision$
+ * @since       PHP 5
+ * @require     PHP 4.0.1 (trigger_error)
+ */
+if (!function_exists('php_strip_whitespace'))
+{
+    function php_strip_whitespace($file)
+    {
+        // Perhaps a better idea would be to use the tokenizer?
+
+
+        // Load file
+        $file = implode('', file($file));
+
+        // Remove whitespace
+        $file = preg_replace('#(<\?php\n?)(.*?)(\?>)#se',
+            "'\\1' . str_replace(\"\n\", ' ', '\\2') . '\\3'",
+            $file);
+
+        // Remove comments
+        $file = preg_replace('#//[^\n]+#s', '', $file);
+        $file = preg_replace('#/\*.*\*/+#su', '', $file);
+
+        return $file;
+    }
+}
+
+?>
