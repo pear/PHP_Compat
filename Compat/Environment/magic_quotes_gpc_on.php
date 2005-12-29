ed33d894730869f8f528db433dfe63458256385c
Arpad Ray <arpad@php.net>
2005-12-29 09:01:10 +0000

    initial implementation
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@203894 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Environment/magic_quotes_gpc_on.php
+++ Compat/Environment/magic_quotes_gpc_on.php
@@ -12,7 +12,7 @@
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Authors: Aidan Lister <aidan@php.net>                                |
+// | Authors: Arpad Ray <arpad@php.net>                                   |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -23,7 +23,32 @@
  *
  * @category    PHP
  * @package     PHP_Compat
- * @link        http://php.net/magic_qutoes
- * @author      Aidan Lister <aidan@php.net>
+ * @link        http://php.net/magic_quotes
+ * @author      Arpad Ray <arpad@php.net>
  * @version     $Revision$
  */
+if (get_magic_quotes_gpc()) {
+    // Recursive addslashes function
+    function php_compat_addslashesr($value, $keybug)
+    {
+        if (!is_array($value)) {
+            return addslashes($value);
+        }
+        $result = array();
+        foreach ($value as $k => $v) {
+            $result[$keybug ? $k : addslashes($k)] = php_compat_addslashesr($v, $keybug);
+        }
+        return $result;
+    }
+
+    // between 5.0.0 and 5.1.0, array keys in the superglobals were escaped even with register_globals off
+    $keybug = (version_compare(PHP_VERSION, '5.0.0', '>=') && version_compare(PHP_VERSION, '5.1.0', '<'));
+
+    $inputs = array(&$_POST, &$_GET, &$_COOKIE);
+    foreach ($inputs as $input) {
+        $inputs[$k] = array_map('php_compat_addslashesr', $v, $keybug);
+    }
+
+    // Register the change
+    ini_set('magic_quotes_gpc', 'on');
+}
