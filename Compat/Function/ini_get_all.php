e28a64683414ce038ee9f7797591de22d1802593
Aidan Lister <aidan@php.net>
2005-12-29 02:17:53 +0000

    More error checking
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@203863 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/ini_get_all.php
+++ Compat/Function/ini_get_all.php
@@ -32,7 +32,7 @@
 function php_compat_ini_get_all($extension = null)
 {
     // Sanity check
-    if (!is_scalar($extension)) {
+    if ($extension !== null && !is_scalar($extension)) {
         user_error('ini_get_all() expects parameter 1 to be string, ' .
             gettype($extension) . ' given', E_USER_WARNING);
         return false;
@@ -44,11 +44,24 @@ function php_compat_ini_get_all($extension = null)
     $info = ob_get_contents();
     ob_clean();
     $info = explode("\n", $info);
-    $line = array_values(preg_grep('#php.ini#', $info));
-    list (, $value) = explode('<td class="v">', $line[0]);
-    $inifile = trim(strip_tags($value));
+    $line = array_values(preg_grep('#php\.ini#', $info));
 
-    // Parse
+    // Plain vs HTML output
+    if (substr($line[0], 0, 4) === '<tr>') {
+        list (, $value) = explode('<td class="v">', $line[0], 2);
+        $inifile = trim(strip_tags($value));
+    } else {
+        list (, $value) = explode(' => ', $line[0], 2);
+        $inifile = trim($value);
+    }
+
+    // Check the file actually exists
+    if (!file_exists($inifile)) {
+        user_error('ini_get_all() Unable to find php.ini', E_USER_WARNING);
+        return false;
+    }
+
+    // Parse the ini
     if ($extension !== null) {
         $ini_all = parse_ini_file($inifile, true);
 
@@ -57,7 +70,14 @@ function php_compat_ini_get_all($extension = null)
             $ini_arr[strtolower($key)] = $value;
         }
 
-        $ini = $ini_arr[$extension];
+        // Check the extension exists
+        if (isset($ini_arr[$extension])) {
+            $ini = $ini_arr[$extension];
+        } else {
+            user_error("ini_get_all() Unable to find extension '$extension'",
+                E_USER_WARNING);
+            return false;
+        }
     } else {
         $ini = parse_ini_file($inifile);
     }
