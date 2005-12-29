1e156b3d1a1efd81206ca7ebe868dc96119cc869
Arpad Ray <arpad@php.net>
2005-12-29 03:40:14 +0000

    use data callback function
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@203877 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/array_udiff_uassoc.php
+++ Compat/Function/array_udiff_uassoc.php
@@ -32,18 +32,28 @@
 function php_compat_array_udiff_uassoc()
 {
     $args = func_get_args();
-    if (count($args) < 3) {
+    if (count($args) < 4) {
         user_error('Wrong parameter count for array_udiff_uassoc()', E_USER_WARNING);
         return;
     }
 
-    // Get compare function
-    $compare_func = array_pop($args);
-    if (!is_callable($compare_func)) {
-        if (is_array($compare_func)) {
-            $compare_func = $compare_func[0] . '::' . $compare_func[1];
+    // Get key compare function
+    $key_compare_func = array_pop($args);
+    if (!is_callable($key_compare_func)) {
+        if (is_array($key_compare_func)) {
+            $key_compare_func = $key_compare_func[0] . '::' . $key_compare_func[1];
         }
-        user_error('array_udiff_uassoc() Not a valid callback ' . $compare_func, E_USER_WARNING);
+        user_error('array_udiff_uassoc() Not a valid callback ' . $key_compare_func, E_USER_WARNING);
+        return;
+    }
+
+    // Get data compare function
+    $data_compare_func = array_pop($args);
+    if (!is_callable($data_compare_func)) {
+        if (is_array($data_compare_func)) {
+            $data_compare_func = $data_compare_func[0] . '::' . $data_compare_func[1];
+        }
+        user_error('array_udiff_uassoc() Not a valid callback ' . $data_compare_func, E_USER_WARNING);
         return;
     }
 
@@ -62,12 +72,12 @@ function php_compat_array_udiff_uassoc()
     foreach ($args[0] as $key => $value) {
         // Check all arrays
         for ($i = 1; $i < $count; $i++) {
-            if (!array_key_exists($key, $args[$i])) {
-                continue;
-            }
-            $result = call_user_func($compare_func, $value, $args[$i][$key]);
-            if ($result === 0) {
-                continue 2;
+            foreach ($args[$i] as $args_key => $args_data) {
+                $key_result = call_user_func($key_compare_func, $key, $args_key);
+                $data_result = call_user_func($data_compare_func, $value, $args_data);
+                if ($key_result === 0 && $data_result === 0) {
+                    continue 3;
+                }
             }
         }
 
