e417e4f83bb06ad852541339718431ef8eac01f5
Arpad Ray <arpad@php.net>
2006-02-04 22:36:13 +0000

    implementation and simple tests for mkdir
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@206427 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/mkdir.php
+++ Compat/Function/mkdir.php
@@ -12,7 +12,7 @@
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Authors: Aidan Lister <aidan@php.net>                                |
+// | Authors: Arpad Ray <arpad@php.net>                                   |
 // +----------------------------------------------------------------------+
 //
 // $Id$
@@ -21,17 +21,42 @@
 /**
  * Replace mkdir()
  *
+ * Stream contexts aren't supported prior to PHP 5, reverts
+ * to native function (to support contexts) in PHP 5+.
+ *
  * @category    PHP
  * @package     PHP_Compat
  * @link        http://php.net/function.mkdir
- * @author      Aidan Lister <aidan@php.net>
+ * @author      Arpad Ray <arpad@php.net>
  * @version     $Revision$
- * @since       PHP 5.0.0 (Added optional recursive parameter)
+ * @since       PHP 5.0.0 (Added optional recursive and context parameters)
  * @require     PHP 4.0.0 (user_error)
  */
-function php_compat_mkdir($pathname, $mode, $recursive = false, $context = null)
-{ 
-    // Finish me
+function php_compat_mkdir($pathname, $mode = 0777, $recursive = true, $context = null) {
+    if (version_compare(PHP_VERSION, '5.0.0', 'gte')) {
+        // revert to native function
+        return (func_num_args() > 3)
+            ? mkdir($pathname, $mode, $recursive, $context)
+            : mkdir($pathname, $mode, $recursive);
+    }
+    if (!strlen($pathname)) {
+        user_error('No such file or directory', E_USER_WARNING);
+        return false;
+    }
+    if (is_dir($pathname)) {
+        if (func_num_args() == 5) {
+            // recursive call
+            return true;
+        }
+        user_error('File exists', E_USER_WARNING);
+        return false;
+    }
+    $parent_is_dir = php_compat_mkdir(dirname($pathname), $mode, $recursive, null, 0);
+    if ($parent_is_dir) {
+        return mkdir($pathname, $mode);
+    }
+    user_error('No such file or directory', E_USER_WARNING);
+    return false;
 }
 
 
