f4dd6e3c9897c6471ad40c5a005dbd1021b98a73
Arpad Ray <arpad@php.net>
2007-04-17 10:09:56 +0000

    Changing license from PHP to LGPL
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@233930 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/is_callable.php
+++ Compat/Function/is_callable.php
@@ -5,6 +5,8 @@
  *
  * @category    PHP
  * @package     PHP_Compat
+ * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
  * @link        http://php.net/function.is_callable
  * @author      Gaetano Giunta <giunta.gaetano@sea-aeroportimilano.it>
  * @author      Arpad Ray <arpad@php.net>
