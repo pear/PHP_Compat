2080086fc8f12923d117b1ff6a3ef57deb5b39ac
Arpad Ray <arpad@php.net>
2009-01-24 22:41:41 +0000

    fix htmlspecialchars - #15540
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@274499 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat/Function/htmlspecialchars.php
+++ Compat/Function/htmlspecialchars.php
@@ -9,11 +9,15 @@
  * @link        http://php.net/function.htmlspecialchars
  * @author      Aidan Lister <aidan@php.net>
  * @version     $Revision$
- * @since       PHP 5.1.0
+ * @since       PHP 4.0.0
  * @require     PHP 4.0.0 (user_error)
  */
-function php_compat_htmlspecialchars($string, $quote_style = null, $charset = null, $double_encode = true)
+function php_compat_htmlspecialchars($string, $quote_style = ENT_COMPAT, $charset = 'ISO-8859-1', $double_encode = true)
 {
+    if (version_compare(PHP_VERSION, '5.2.3', 'ge')) {
+        return htmlspecialchars($string, $quote_style, $charset, $double_encode);
+    }
+
     // Sanity check
     if (!is_scalar($string)) {
         user_error('htmlspecialchars() expects parameter 1 to be string, ' .
@@ -38,31 +42,87 @@ function php_compat_htmlspecialchars($string, $quote_style = null, $charset = nu
 				   gettype($double_encode) . ' given', E_USER_WARNING);
         return;
     }
-	
-	if ($double_encode === true) {
-		$string = str_replace('&amp;', '&', $string);
-	}
-	
-	$tf = array('&' => '&amp;',
-				'<' => '&lt;',
-				'>' => '&gt;');
-	
-	if ($quote_style & ENT_NOQUOTES) {
-		$tf['"'] = '&quot';
-	}
-	
-	if ($quote_style & ENT_QUOTES) {
-		$tf["'"] = '&#039;';
-	}
-	
-    return str_replace(array_keys($tf), array_values($tf), $string);
-}
-
-
-// Define
-if (!function_exists('htmlspecialchars')) {
-    function htmlspecialchars($string, $quote_style = null, $charset = null, $double_encode = true)
-    {
-        return php_compat_htmlspecialchars($string, $quote_style, $charset, $double_encode);
+    
+    // mb support
+    if ($charset != 'ISO-8859-1') {
+        if (!function_exists('mb_substr')) {
+            user_error('php_compat_htmlspecialchars requires PHP >= 4.0.6 and '
+                . 'the mbstring extension to support the $charset argument.',
+                E_USER_WARNING);
+            return;
+        }
+        $len = mb_strlen($string, $charset);
+        $ret = '';
+        for ($i = 0; $i < $len; $i++) {
+            $char = mb_substr($string, $i, 1, $charset);
+            
+            switch ($char) {
+            case '&':
+                if (!$double_encode && $i < $len - 2) {
+                    // look ahead to see if we have an existing entity
+                    $foundEntity = false;
+                    $type = mb_substr($string, $i + 1, 1, $charset);
+                    if ($type == '#') {
+                        // numeric entities
+                        $type2 = mb_substr($string, $i + 2, 1, $charset);
+                        if ($type2 == 'x') {
+                            $validator = 'ctype_xdigit';
+                        } else if (ctype_digit($type2)) {
+                            $validator = 'ctype_digit';
+                            $foundEntity = true;
+                        } else {
+                            // invalid entity
+                            $ret .= '&amp;';
+                            break;
+                        }
+                    } else if (ctype_alnum($type)) {
+                        $validator = 'ctype_alnum';
+                        $foundEntity = true;
+                    } else {
+                        $ret .= '&amp;';
+                        break;
+                    }
+                    for ($j = $i + ($type == '#' ? 3 : 2); $j < $len; $j++) {
+                        $tempChar = mb_substr($string, $j, 1, $charset);
+                        if ($foundEntity && $tempChar == ';') {
+                            $ret .= mb_substr($string, $i, $j - $i + 1, $charset);
+                            $i = $j;
+                            break 2;
+                        }
+                        if ($validator($tempChar)) {
+                            $foundEntity = true;
+                        } else {
+                            // invalid entity
+                            $ret .= '&amp;';
+                            break;
+                        }
+                    }
+                }
+                $ret .= '&amp;';
+                break;
+            case '"':
+                $ret .= $quote_style & ENT_NOQUOTES ? '"' : '&quot;';
+                break;
+            case "'":
+                $ret .= $quote_style & ENT_COMPAT || $quote_style & ENT_NOQUOTES ? "'" : '&#039;';
+                break;
+            case '<':
+                $ret .= '&lt;';
+                break;
+            case '>':
+                $ret .= '&gt;';
+                break;
+            default:
+                $ret .= $char;
+                break;
+            }
+        }
+        return $ret;
     }
+	if (!$double_encode) {
+		return preg_replace('/[^&]|&(?!(?:#(?:x[a-f\d]+|\d+)|\w+);)/e', 'htmlspecialchars("$0", $quote_style)', $string);
+	}
+	
+    return htmlspecialchars($string, $quote_style);
 }
+
