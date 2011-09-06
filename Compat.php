242f54f1601ca239235693905eeeb7a32fe27c25
James Wade <hm2k@php.net>
2011-09-06 20:13:55 +0000

    Fixed Compat.php: Request #15139	PHP_Compat does not comform to PEAR CS
    git-svn-id: https://svn.php.net/repository/pear/packages/PHP_Compat/trunk@316290 c90b9560-bf6c-de11-be94-00142212c4b1

--- Compat.php
+++ Compat.php
@@ -1,26 +1,44 @@
 <?php
+
 /**
+ * PHP Compat
+ *
  * Provides missing functionality in the form of constants and functions
  *   for older versions of PHP
  *
  * Optionally, you may simply include the file.
  *   e.g. require_once 'PHP/Compat/Function/scandir.php';
  *
- * @category    PHP
- * @package     PHP_Compat
- * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
- * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
- * @version     $Revision$
- * @author      Aidan Lister <aidan@php.net>
- * @static
+ * PHP version 4
+ *
+ * @category  PHP
+ * @package   PHP_Compat
+ * @author    Aidan Lister <aidan@php.net>
+ * @copyright 2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @license   LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @version   SVN: $Id$
+ * @link      http://pear.php.net/package/PHP_Compat
+ */
+
+/**
+ * Provides components to achieve PHP version independence
+ *
+ * @category  PHP
+ * @package   PHP_Compat
+ * @author    Aidan Lister <aidan@php.net>
+ * @copyright 2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @license   LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @version   Release: 1.7.2
+ * @link      http://pear.php.net/package/PHP_Compat
  */
 class PHP_Compat
 {
     /**
      * Load a function, or array of functions
      *
-     * @param   string|array    $function   The function or functions to load
-     * @return  bool|array      TRUE if loaded, FALSE if not
+     * @param string|array $function The function or functions to load
+     *
+     * @return bool|array   TRUE if loaded, FALSE if not
      */
     function loadFunction($function)
     {
@@ -30,24 +48,20 @@ class PHP_Compat
             foreach ($function as $singlefunc) {
                 $res[$singlefunc] = PHP_Compat::loadFunction($singlefunc);
             }
-
             return $res;
         }
-
         // Check for packages which can modify the function table at runtime
         $symbolfuncs = array('rename_function', 'runkit_rename_function');
         foreach ($symbolfuncs as $symbolfunc) {
             $renamedfunction = 'php_compat_renamed' . $function;
-            if (function_exists($symbolfunc) &&
-                function_exists($function) &&
-                !function_exists($renamedfunction)) {
-
-                // Rename the core function
-                rename_function($function, $renamedfunction);
-                break;
+            if (function_exists($symbolfunc) && function_exists($function)
+                && !function_exists($renamedfunction)
+            ) {
+                    // Rename the core function
+                    rename_function($function, $renamedfunction);
+                    break;
             }
         }
-
         // Single
         if (!function_exists($function)) {
             $file = sprintf('PHP/Compat/Function/%s.php', $function);
@@ -55,16 +69,14 @@ class PHP_Compat
                 return true;
             }
         }
-
         return false;
     }
-
-
     /**
      * Load a constant, or array of constants
      *
-     * @param   string|array    $constant   The constant or constants to load
-     * @return  bool|array      TRUE if loaded, FALSE if not
+     * @param string|array $constant The constant or constants to load
+     *
+     * @return bool|array   TRUE if loaded, FALSE if not
      */
     function loadConstant($constant)
     {
@@ -74,26 +86,22 @@ class PHP_Compat
             foreach ($constant as $singleconst) {
                 $res[$singleconst] = PHP_Compat::loadConstant($singleconst);
             }
-
             return $res;
         }
-
         // Single
         $file = sprintf('PHP/Compat/Constant/%s.php', $constant);
         if ((@include_once $file) !== false) {
             return true;
         }
-
         return false;
     }
-
-
     /**
      * Load an environment
      *
-     * @param   string          $environment   The environment to load
-     * @param   string          $setting       Turn the environment on or off
-     * @return  bool            TRUE if loaded, FALSE if not
+     * @param string $environment The environment to load
+     * @param string $setting     Turn the environment on or off
+     *
+     * @return bool   TRUE if loaded, FALSE if not
      */
     function loadEnvironment($environment, $setting)
     {
@@ -102,46 +110,39 @@ class PHP_Compat
         if ((@include_once $file) !== false) {
             return true;
         }
-
         return false;
     }
-
-
     /**
      * Load components for a PHP version
      *
-     * @param   string      $version        PHP Version to load
-     * @return  array       An associative array of component names loaded
+     * @param string $version PHP Version to load
+     *
+     * @return array  An associative array of component names loaded
      */
     function loadVersion($version = null)
     {
         // Include list of components
-        require 'PHP/Compat/Components.php';
-
+        include 'PHP/Compat/Components.php';
         // Include version_compare to work with older versions
         PHP_Compat::loadFunction('version_compare');
-
         // Init
-        $phpversion = phpversion();
-        $methods = array(
-            'function' => 'loadFunction',
-            'constant' => 'loadConstant');
+        $phpver = phpversion();
+        $methods = array('function' => 'loadFunction', 'constant' => 'loadConstant');
         $res = array();
-
         // Iterate each component
         foreach ($components as $type => $slice) {
-            foreach ($slice as $component => $compversion) {
-                if (($version === null &&
-                        1 === version_compare($compversion, $phpversion)) ||    // C > PHP
-                       (0 === version_compare($compversion, $version) ||        // C = S
-                        1 === version_compare($compversion, $phpversion))) {    // C > PHP
-
-                    $res[$type][$component] =
-                        call_user_func(array('PHP_Compat', $methods[$type]), $component);
+            foreach ($slice as $component => $cver) {
+                $vercom = version_compare($cver, $phpver);
+                if (($version === null && $vercom === 1)  // C > PHP
+                    || (version_compare($cver, $version) === 0 // C = S
+                    || $vercom === 1)
+                ) { // C > PHP
+                        $res[$type][$component] = call_user_func(
+                            array('PHP_Compat', $methods[$type]), $component
+                        );
                 }
             }
         }
-
         return $res;
     }
 }
